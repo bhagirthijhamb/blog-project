22b3ef93f1499135de967af6e9dacbcc0bc4854c
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-11-17 13:54:06 -0500

    added route to add blogs

--- routes/blogs.js
+++ routes/blogs.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const Blog = require('./../models/Blog');
 
 router.route('/').get(async(req, res) => {
-    // const { title, body, author, comments } = req.body
     const blogs = await Blog.find();
     res.json(blogs);
+})
+
+router.route('/add').post(async(req, res) => {
+    const { title, body, author, comments } = req.body
+    const blog = new Blog({
+        title,
+        body,
+        author
+    });
+    
+    const doc = await blog.save();
+    res.json(doc);
 })
 
 module.exports = router;
